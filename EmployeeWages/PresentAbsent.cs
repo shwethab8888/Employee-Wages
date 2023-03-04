574127fd6b17ebc04392535b16e6d5c6ee73f8d9
shwethab8888 <shwethab8888@gmail.com>
2023-03-04 07:04:52 +0530

    Employee present or absent

---
+++ EmployeeWages/PresentAbsent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    public class PresentAbsent
+    {
+        public static void Present_Absent()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            // generates a random integer between 0 and 2
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+            }
+        }
+    }
+}
---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Employee Wage Program");
+            PresentAbsent.Present_Absent();
+
+        }
+    }
+}
